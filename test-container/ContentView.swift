9b5ffda61717f6b5b257e8a3d18318f5453ee748
Tamas Papik <trapacska@gmail.com>
2020-02-20 12:47:43 +0100

    initial

---
+++ test-container/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test-container
+//
+//  Created by Tamás Papik on 2020. 02. 20..
+//  Copyright © 2020. Bitrise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
